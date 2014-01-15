32154616fcbd379503dbdf3bc78847cf85cd1ef5
Chip Bell <chipbell4@gmail.com>
2014-01-15 00:51:48 -0500

    Finding bugs in notes, rendering empty list

--- app.js
+++ app.js
@@ -13,4 +13,40 @@ var LinkCollection = Backbone.Collection.extend({
 
 });
 
+/*
+ * The View
+ */
+var LinkView = Backbone.View.extend({
 
+	initialize: function() {
+		
+		this.collection = new LinkCollection();
+
+	},
+
+	render: function() {
+		// munge together template code
+		var template_func = _.template( $('#single-row-template').html() );
+		var new_html = '';
+		var N = this.collection.size();
+
+		for(var i=0; i<N; i++) {
+			var model = this.collection.at(i);
+			
+			new_html += template_func( model.toJSON() );
+		}
+
+		// render it to the container
+		$('#row-container').html( new_html );
+
+	}
+
+});
+
+
+$(document).ready(function() {
+	
+	var view = new LinkView();
+	view.render();
+
+});
